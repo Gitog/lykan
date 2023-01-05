57ef7ed7a1138f172f8edbb2de73891e5eda4aea
luckyligovi <lucky.luvihi@student.moringaschool.com>
2023-01-05 10:30:57 +0300

    products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,16 +3,123 @@ import '../css/products.css'
 
 export default function Products() {
   return (
-    <div className='card'>
-      <div className='imageclass'>
-        <img src='https://cdn.pixabay.com/photo/2015/04/09/19/53/sock-715022_960_720.jpg' alt="Product image"/>
+    <div className='row'> 
+      <div className='column'>
+        <div className='card'>
+          <div className='imageclass'>
+            <img src='https://cdn.pixabay.com/photo/2015/04/09/19/53/sock-715022_960_720.jpg' alt="Product image"/>
+          </div>
+        <div className='content'>
+          <h2>Socks</h2>
+          <p>Carter's 6-Pack Crew Socks Navy/White/Grey Age-4 Years & Above-3H798510</p>
+          <p className="price">Kshs 1,200</p>
+          <button>Add to Cart</button>
+          </div>
+        </div>
       </div>
-     <div className='content'>
-      <h2>Socks</h2>
-      <p>Carter's 6-Pack Crew Socks Navy/White/Grey Age-4 Years & Above-3H798510</p>
-      <p>Kshs 1,200</p>
-      <button>Add to Cart</button>
+      
+      <div className='column'>
+        <div className='card'>
+          <div className='imageclass'>
+            <img src='https://cdn.pixabay.com/photo/2015/04/09/19/53/sock-715022_960_720.jpg' alt="Product image"/>
+          </div>
+        <div className='content'>
+          <h2>Socks</h2>
+          <p>Carter's 6-Pack Crew Socks Navy/White/Grey Age-4 Years & Above-3H798510</p>
+          <p className="price">Kshs 1,200</p>
+          <button>Add to Cart</button>
+          </div>
+        </div>
       </div>
-    </div>
+
+      <div className='column'>
+        <div className='card'>
+          <div className='imageclass'>
+            <img src='https://cdn.pixabay.com/photo/2015/04/09/19/53/sock-715022_960_720.jpg' alt="Product image"/>
+          </div>
+        <div className='content'>
+          <h2>Socks</h2>
+          <p>Carter's 6-Pack Crew Socks Navy/White/Grey Age-4 Years & Above-3H798510</p>
+          <p className="price">Kshs 1,200</p>
+          <button>Add to Cart</button>
+          </div>
+        </div>
+      </div>
+
+      <div className='column'>
+        <div className='card'>
+          <div className='imageclass'>
+            <img src='https://cdn.pixabay.com/photo/2015/04/09/19/53/sock-715022_960_720.jpg' alt="Product image"/>
+          </div>
+        <div className='content'>
+          <h2>Socks</h2>
+          <p>Carter's 6-Pack Crew Socks Navy/White/Grey Age-4 Years & Above-3H798510</p>
+          <p className="price">Kshs 1,200</p>
+          <button>Add to Cart</button>
+          </div>
+        </div>
+      </div>
+
+      <div className='column'>
+        <div className='card'>
+          <div className='imageclass'>
+            <img src='https://cdn.pixabay.com/photo/2015/04/09/19/53/sock-715022_960_720.jpg' alt="Product image"/>
+          </div>
+        <div className='content'>
+          <h2>Socks</h2>
+          <p>Carter's 6-Pack Crew Socks Navy/White/Grey Age-4 Years & Above-3H798510</p>
+          <p className="price">Kshs 1,200</p>
+          <button>Add to Cart</button>
+          </div>
+        </div>
+      </div>
+
+      <div className='column'>
+        <div className='card'>
+          <div className='imageclass'>
+            <img src='https://cdn.pixabay.com/photo/2015/04/09/19/53/sock-715022_960_720.jpg' alt="Product image"/>
+          </div>
+        <div className='content'>
+          <h2>Socks</h2>
+          <p>Carter's 6-Pack Crew Socks Navy/White/Grey Age-4 Years & Above-3H798510</p>
+          <p className="price">Kshs 1,200</p>
+          <button>Add to Cart</button>
+          </div>
+        </div>
+      </div>
+
+      <div className='column'>
+        <div className='card'>
+          <div className='imageclass'>
+            <img src='https://cdn.pixabay.com/photo/2015/04/09/19/53/sock-715022_960_720.jpg' alt="Product image"/>
+          </div>
+        <div className='content'>
+          <h2>Socks</h2>
+          <p>Carter's 6-Pack Crew Socks Navy/White/Grey Age-4 Years & Above-3H798510</p>
+          <p className="price">Kshs 1,200</p>
+          <button>Add to Cart</button>
+          </div>
+        </div>
+      </div>
+
+      <div className='column'>
+        <div className='card'>
+          <div className='imageclass'>
+            <img src='https://cdn.pixabay.com/photo/2015/04/09/19/53/sock-715022_960_720.jpg' alt="Product image"/>
+          </div>
+        <div className='content'>
+          <h2>Socks</h2>
+          <p>Carter's 6-Pack Crew Socks Navy/White/Grey Age-4 Years & Above-3H798510</p>
+          <p className="price">Kshs 1,200</p>
+          <button>Add to Cart</button>
+          </div>
+        </div>
+      </div>
+    </div> 
   )
+
+
+  
+
+  
 }
