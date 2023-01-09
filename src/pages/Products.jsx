b96eca777d552e219f64f2da24c2b654c540bba9
luckyligovi <lucky.luvihi@student.moringaschool.com>
2023-01-09 15:10:07 +0300

    sidebar

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,7 +3,26 @@ import '../css/products.css'
 
 export default function Products() {
   return (
-    <div className='row'> 
+  <div className='shop'>
+    <div className='sidebar'>
+      <button>search</button>
+      <h1>Sale is Live</h1>
+      <div className="columnSide">
+        <div className="cardSide">
+          <div className="imageSide">
+            <img src='https://ankarashoes.store/wp-content/uploads/2022/05/23-men.png' alt="shoe image"/>
+          </div>
+        <div className='content'>
+          <h2>shoes</h2>
+          <p className="price"> Kshs {(1200 )} </p>
+          
+          </div>
+        </div>
+      </div>
+      
+    </div>
+   <div className='row'>    
+    <h1>shop products</h1>
       <div className='column'>
         <div className='card'>
           <div className='imageclass'>
@@ -49,16 +68,30 @@ export default function Products() {
       <div className='column'>
         <div className='card'>
           <div className='imageclass'>
+            <img src='https://ankarashoes.store/wp-content/uploads/2022/05/23-men.png' alt="shoe image"/>
+          </div>
+        <div className='content'>
+          <h2>shoes</h2>
+          <p>Carter's 6-Pack Crew Socks Navy/White/Grey Age-4 Years & Above-3H798510</p>
+          <p className="price"> Kshs {(1200 )} </p>
+          <button>Add to Cart</button>
+          </div>
+        </div>
+      </div>
+
+      <div className='column'>
+     <div className='card'>
+          <div className='imageclass'>
             <img src='https://cdn.pixabay.com/photo/2015/04/09/19/53/sock-715022_960_720.jpg' alt="Product image"/>
           </div>
         <div className='content'>
           <h2>Socks</h2>
           <p>Carter's 6-Pack Crew Socks Navy/White/Grey Age-4 Years & Above-3H798510</p>
-          <p className="price">Kshs 1,200</p>
+          <p className="price">Kshs {(1,200)}</p>
           <button>Add to Cart</button>
           </div>
         </div>
-      </div>
+      </div> 
 
       <div className='column'>
         <div className='card'>
@@ -91,21 +124,7 @@ export default function Products() {
       <div className='column'>
         <div className='card'>
           <div className='imageclass'>
-            <img src='https://cdn.pixabay.com/photo/2015/04/09/19/53/sock-715022_960_720.jpg' alt="Product image"/>
-          </div>
-        <div className='content'>
-          <h2>Socks</h2>
-          <p>Carter's 6-Pack Crew Socks Navy/White/Grey Age-4 Years & Above-3H798510</p>
-          <p className="price">Kshs 1,200</p>
-          <button>Add to Cart</button>
-          </div>
-        </div>
-      </div>
-
-      <div className='column'>
-        <div className='card'>
-          <div className='imageclass'>
-            <img src='https://cdn.pixabay.com/photo/2015/04/09/19/53/sock-715022_960_720.jpg' alt="Product image"/>
+            <img src='https://cdn.pixabay.com/photo/2015/04/09/19/53/sock-715022_960_720.jpg' alt="socks image"/>
           </div>
         <div className='content'>
           <h2>Socks</h2>
@@ -116,7 +135,7 @@ export default function Products() {
         </div>
       </div>
     </div> 
-  )
+  </div>)
 
 
   
