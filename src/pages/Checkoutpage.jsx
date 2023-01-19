be6536559aeb85c33461f7d6da08beccadff8bbd
GordonGichini <gordongichini1@gmail.com>
2023-01-19 23:30:24 +0300

    checkout styling

--- src/pages/Checkoutpage.jsx
+++ src/pages/Checkoutpage.jsx
@@ -1,52 +1,52 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../css/checkoutpage.css';
+import { useSelector, useDispatch } from "react-redux";
 
-const CheckoutPage = () => {
+
+function CheckoutPage() { 
+  const items = useSelector(state => state.products.cart);
+  console.log(items)
+
+  const dispatch = useDispatch();
+
+  const handlePlaceOrder = () => {
+    alert("Order placed successfully!");
+  };
+  const renderedItems = items.map(item => (
+    <div key={item.id}>
+      <p>{item.name}</p>
+      <p>Price: ${item.price}</p>
+      </div>
+  )); 
+
+  const totalCost = items.reduce((acc, item) => acc + parseInt(item.price), 0);
+
   return (
     <div className="checkout">
-      <div className="details">
-        <h1>Checkout</h1>
-        <h2>Billing Details </h2>
-        <form>
-          <label htmlFor="name">Name:</label>
-          <input type="text" id="name" />
-          <br />
-          <label htmlFor="lastname">Last name(optional)</label>
-          <input type="text" id="lastname" />
-          <br />
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" />
-          <br />
-          <label htmlFor="address">Address:</label>
-          <input type="text" id="address" />
-          <br />
-          <label htmlFor="telephone">Phone no:</label>
-          <input type="text" id="telephone" />
-          <br />
-        </form>
-      </div>
-
-      <div className="orders">
-        <h2> Order Summary</h2>
-        <div className='order'>
-          <div>Product 1</div>
-          <div>$10</div>
-        </div>
-        <div className='order'>
-          <div>Product 2</div>
-          <div>$20</div>
-        </div>
-        <div className='order'>
-          <div>Total:</div>
-          <div>$30</div>
-        </div>
-        <button type="submit">Place Order</button>
-      </div>
-
+      <h1>Checkout</h1>
+      <h2>Billing Details </h2>
+      <form>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" />
+        <br />
+        <label htmlFor="lastname">Last name(optional)</label>
+        <input type="text" id="lastname" />
+        <br />
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" />
+        <br />
+        <label htmlFor="address">Address:</label>
+        <input type="text" id="address" />
+        <br />
+        <label htmlFor="telephone">Phone no:</label>
+        <input type="text" id="telephone" />
+        <br />
+      </form>
+     <h3> Order Summary </h3>
+        {renderedItems}
+        <p>Total Cost: ${totalCost}</p><button onClick={handlePlaceOrder}>Place Order</button>
     </div>
-
-
-  );
+  ); 
 };
 
 export default CheckoutPage;
